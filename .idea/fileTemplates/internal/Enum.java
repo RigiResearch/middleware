03cd08e965a0ebd65380867f6a4009aeabf0a196
Miguel Jimenez <accounts@migueljimenez.co>
2019-04-28 20:38:20 -0700

    Setup default file JavaDoc

---
+++ .idea/fileTemplates/internal/Enum.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * TODO document this enum.
+ * @author Miguel Jimenez (miguel@uvic.ca)
+ * @version \$Id$
+ * @since 0.1.0
+ */
+public enum ${NAME} {
+}
---
+++ .idea/fileTemplates/internal/package-info.java
@@ -0,0 +1,8 @@
+#parse("File Header.java")
+/**
+ * Contains...
+ * @author Miguel Jimenez (miguel@uvic.ca)
+ * @version \$Id$
+ * @since 0.1.0
+ */
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
