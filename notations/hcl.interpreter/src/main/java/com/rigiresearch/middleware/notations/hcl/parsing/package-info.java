11b41b86df5659dbb9c69516ce27fa4731a115f2
Miguel Jiménez <migueljimenezachinte@gmail.com>
2019-10-08 13:22:02 -0700

    Create a parser to instantiate the AST based on an HCL specification #2

---
+++ notations/hcl.interpreter/src/main/java/com/rigiresearch/middleware/notations/hcl/parsing/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains classes related to the HCL parser.
+ * @author Miguel Jimenez (miguel@uvic.ca)
+ * @version $Id$
+ * @since 0.1.0
+ */
+package com.rigiresearch.middleware.notations.hcl.parsing;
