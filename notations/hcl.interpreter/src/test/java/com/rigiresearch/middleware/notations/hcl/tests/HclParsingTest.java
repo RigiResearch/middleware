be439038dec242331e817af6f90197eb7a7a129e
Miguel Jimenez <accounts@migueljimenez.co>
2019-10-10 13:32:38 -0700

    Add support for resource and attribute comments

--- notations/hcl.interpreter/src/test/java/com/rigiresearch/middleware/notations/hcl/tests/HclParsingTest.java
+++ notations/hcl.interpreter/src/test/java/com/rigiresearch/middleware/notations/hcl/tests/HclParsingTest.java
@@ -1,10 +1,14 @@
 package com.rigiresearch.middleware.notations.hcl.tests;
 
 import com.google.inject.Inject;
+import com.rigiresearch.middleware.metamodels.hcl.Dictionary;
+import com.rigiresearch.middleware.metamodels.hcl.NameValuePair;
+import com.rigiresearch.middleware.metamodels.hcl.Resource;
 import com.rigiresearch.middleware.metamodels.hcl.Specification;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.xtext.testing.InjectWith;
 import org.eclipse.xtext.testing.extensions.InjectionExtension;
 import org.eclipse.xtext.testing.util.ParseHelper;
@@ -35,7 +39,8 @@ class HclParsingTest {
             + "}";
         final Specification specification = this.helper.parse(source);
         Assertions.assertNotNull(specification);
-        final EList<Resource.Diagnostic> errors = specification.eResource().getErrors();
+        final EList<org.eclipse.emf.ecore.resource.Resource.Diagnostic> errors =
+            specification.eResource().getErrors();
         Assertions.assertTrue(
             errors.isEmpty(),
             String.format(
@@ -44,6 +49,39 @@ class HclParsingTest {
                     .map(e -> e.toString())
                     .collect(Collectors.joining())
             )
+        );
+    }
+
+    @Test
+    void testComments() throws Exception {
+        final List<String> lines = Arrays.asList(
+            "# A first comment",
+            "# A second comment"
+        );
+        final String comments = String.format("%s\n", String.join("\n", lines));
+        final String source = comments
+            + "variable \"node_count\" {\n"
+            + comments
+            + "\tdefault = 1\n"
+            + "}";
+        final Specification specification = this.helper.parse(source);
+        final Resource variable = specification.getResources().get(0);
+        final NameValuePair attribute = ((Dictionary) variable.getValue())
+            .getElements()
+            .get(0);
+        Assertions.assertNotNull(
+            specification,
+            "The specification couldn't be parsed"
+        );
+        Assertions.assertEquals(
+            lines.size(),
+            variable.getComment().getLines().size(),
+            "Unexpected number of resource comments"
+        );
+        Assertions.assertEquals(
+            lines.size(),
+            attribute.getComment().getLines().size(),
+            "Unexpected number of attribute comments"
         );
     }
 
